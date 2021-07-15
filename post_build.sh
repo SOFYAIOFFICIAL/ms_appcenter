4d8b20287b0c952a9bff119aa1e70692d676c725
SOFY AI <86354806+SOFYAIOFFICIAL@users.noreply.github.com>
2021-07-14 19:38:25 -0500

    Create post_build.sh

---
+++ post_build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+APK_PATH=$APPCENTER_OUTPUT_DIRECTORY/app-release.apk
+SUBSCRIPTION_KEY="acc80bd1-5cb7-4c89-a01e-2e7169009e52"
+curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+--header "SubscriptionKey: $SUBSCRIPTION_KEY" \
+--form "applicationFile=@"$APK_PATH""
